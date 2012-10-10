4ef8fc630bb0360f921f30c942bccf02e9ddc39c
Andy McGrath <andy@moespad.com>
2012-10-10 00:07:16 -0500

    Initial Commit

---
+++ grails-app/domain/mobile/grails/Author.groovy
@@ -0,0 +1,10 @@
+package mobile.grails
+
+class Author {
+    String firstName
+    String lastName
+    Date birthdate
+
+    static constraints = {
+    }
+}
---
+++ grails-app/domain/mobile/grails/Book.groovy
@@ -0,0 +1,13 @@
+package mobile.grails
+
+import javax.security.sasl.AuthorizeCallback
+
+class Book {
+
+    String title
+    Author author
+
+
+    static constraints = {
+    }
+}
